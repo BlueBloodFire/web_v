96708a5bda2564965f7ada822e6d2a1a273ce8a1
Jin <99856344+BlueBloodFire@users.noreply.github.com>
2024-04-17 13:30:56 +0800

    添加各种网页组件功能初始模板

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,6 +1,50 @@
 import {getRequest} from "@/utils/api";
 import {request} from "axios";
 
+
+let initMenu;
+let formatRoutes;
+export default initMenu = (router, store) => {
+    if (store.state.routes.length > 0) {
+        return;
+    }
+    getRequest("/system/config/menu").then( data => {
+        if (data) {
+            let fmtRoutes = formatRoutes(data)
+            router.addRoutes(fmtRoutes);
+            store.commit('initRoutes', fmtRoutes)
+        }
+    })
+}
+
+export default  formatRoutes = (routes) => {
+    let fmRoutes = [];
+    routes.forEach( router => {
+        let {
+            path,
+            component,
+            name,
+            meta,
+            iconCls,
+            children
+        } = router;
+        if (children && children instanceof Array) {
+            children = formatRoutes(children)
+        }
+        let fmRouter = {
+            path : path,
+            name : name,
+            iconCls : iconCls,
+            meta: meta,
+            children : children,
+            component(resolve) {
+                require(['../view/' + component + '.vue'], resolve)
+            }
+        }
+        fmRoutes.push(fmRouter);
+    })
+    return fmRoutes
+}
 // export default initMenu = (router, store) => {
 //     if (store.state.routes.length > 0) {
 //         return;
